94eb63160fb979c259c76a5b866d6bd77b86b725
bae.jeonghan <jeonghan-bae@united.jp>
2019-02-16 10:03:04 +0000

    add login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,14 @@
 class TasksController < ApplicationController
   
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
     
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+    if logged_in?
+        @task = current_user.tasks.build
+        @tasks = current_user.tasks
+    end
   end
 
   def show
@@ -14,7 +19,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -57,5 +62,11 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-end
+  def correct_user
+    @task = current_user.task.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
+end
